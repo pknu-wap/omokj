e8e66e390a72dc44e6023857298b18ed0397e85c
ldofst10 <leaderofst10@gmail.com>
2020-12-29 13:59:21 +0900

    a

--- src/omok/GUI.java
+++ src/omok/GUI.java
@@ -12,9 +12,14 @@ import clientCommunication.*;
 
 public class GUI extends JFrame  {
 	public GUI() {
-		setSize(700,700); //�겕湲� 吏��젙. �닔�젙 媛��뒫
+		setSize(700,700); //占쎄쾿疫뀐옙 筌욑옙占쎌젟. 占쎈땾占쎌젟 揶쏉옙占쎈뮟
 		setLayout(null);
-		setTitle("오목J"); //�쐢�룄�슦 李� �씠由� 吏��젙, �쓽�끉
+		setTitle("OmokJ"); //占쎌맊占쎈즲占쎌뒭 筌∽옙 占쎌뵠�뵳占� 筌욑옙占쎌젟, 占쎌벥占쎈걠
+		//Toolkit toolkit = Toolkit.getDefaultToolkit();
+		//Image img = toolkit.getImage("omok.gif");
+		ImageIcon icon = new ImageIcon(getClass().getResource("omok.gif"));
+		Image img = icon.getImage();
+		setIconImage(img);
 		
 		GUI_main gui_main = new GUI_main();
 		
--- src/omok_logic/OmokInterface.java
+++
@@ -1,6 +0,0 @@
-package omok_logic;
-
-public interface OmokInterface {
-	  public void viewOmok();
-	  public int OmokAction(int x, int y, int turn);
-	}
